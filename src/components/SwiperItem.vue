a2d2e7797f76331ad06f60087260a377a9dfa525
Jofferson R Tiquez <jofftiquez@gmail.com>
2019-08-21 03:32:31 +0800

    feat: create SwiperItem component

---
+++ src/components/SwiperItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="swipe-me-swipe-container">
+    <h1>Foo</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+
+.swipe-me-swipe-container {
+  min-height: 300px;
+  min-width: 500px;
+  background-color: red;
+}
+</style>
